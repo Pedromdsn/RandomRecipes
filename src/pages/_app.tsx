c3f1f0d7bfc236601e3ff8cdd906fd7f59714502
Pedromdsn <pedromdsn.2002@hotmail.com>
2021-09-06 18:10:07 +0100

    fix: icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,17 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>FootSite</title>
-				<link rel="icon" href="/favicon.ico" />
+				<title>Food Suggestion</title>
+				<link rel="icon" sizes="48x48" href="/favicon.png" />
+				<meta name="description" content="Minecraft host a um preço super gostoso." />
+				<meta name="author" content="Coco Blanco" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Kaisei+HarunoUmi:wght@700&family=Lexend+Giga:wght@500&family=Old+Standard+TT:wght@400;700&family=Lexend+Deca:wght@300display=swap"
+					href="https://fonts.googleapis.com/css2?family=Kaisei+HarunoUmi:wght@700&family=Lexend+Giga:wght@500&family=Old+Standard+TT:wght@400;700&display=swap"
 					rel="stylesheet"
 				/>
+				<link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap" rel="stylesheet"/> 
 			</Head>
 			<Component {...pageProps} />
 		</>
